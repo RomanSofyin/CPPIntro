754d9fd590e1987469662d27d56c49282109e4da
Roman Sofyin <roma.sofin@Gmail.com>
2017-11-11 23:52:14 +0300

    Some further stuff

---
+++ week3/Cls.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+
+struct Cls {
+	Cls(char c, double d, int i) : c(c), d(d), i(i) {}
+private:
+	char c;
+	double d;
+	int i;
+};
---
+++ week3/ClsClone.h
@@ -0,0 +1,9 @@
+#pragma once
+
+struct ClsClone {
+	ClsClone(char c, double d, int i) : c(c), d(d), i(i) {}
+public:
+	char c;
+	double d;
+	int i;
+};
