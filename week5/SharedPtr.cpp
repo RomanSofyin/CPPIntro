e63e4cf223a645fcf1492cd086cd61847c1a4755
Roman Sofyin <roma.sofin@Gmail.com>
2017-12-02 16:03:12 +0300

    SharedPtr implementation is in progress

--- week5/SharedPtr.cpp
+++ week5/SharedPtr.cpp
@@ -6,24 +6,47 @@ SharedPtr::SharedPtr(Expression *ptr) : ptr(ptr) {
 	nRef = (ptr == NULL) ? NULL : new unsigned(1);
 }
 
+SharedPtr::SharedPtr(const SharedPtr &shp) : ptr(shp.ptr), nRef(shp.nRef) {
+	if (ptr)
+		*nRef += 1;
+}
+
 SharedPtr::~SharedPtr() {
-	if (!--(*nRef)) {
+	if (ptr && !--(*nRef)) {
 		delete ptr;
 		delete nRef;
 	}
 }
 
-SharedPtr::SharedPtr(const SharedPtr &shp) : ptr(shp.ptr), nRef(shp.nRef) {
-	*nRef += 1;
-}
-
 SharedPtr& SharedPtr::operator=(const SharedPtr& shp) {
-	if (this != &shp)
-/*		SharedPtr(shp).swap(*this);*/
+	if (this != &shp) {
+		SharedPtr copyThis(shp);
+		copyThis.swap(*this);
+/*		if (copyThis.ptr && *copyThis.nRef == 1)
+			copyThis.~SharedPtr();*/
+	}
 	return *this;
 }
 
 void SharedPtr::swap(SharedPtr & shp) {
 	std::swap(ptr, shp.ptr);
 	std::swap(nRef, shp.nRef);
+}
+
+Expression& SharedPtr::operator*() const {
+	return *ptr;
+}
+
+Expression* SharedPtr::operator->() const {
+	return ptr;
+}
+
+Expression* SharedPtr::get() const {
+	return ptr;
+}
+
+void SharedPtr::reset(Expression *ptr) {
+	if (!--(*nRef))
+		delete this->ptr;
+	this->ptr = ptr;
 }
