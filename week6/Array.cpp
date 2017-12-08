1fb3975426be2b4de7495f8099a503294236a250
Roman Sofyin <roma.sofin@Gmail.com>
2017-12-08 12:10:56 +0300

    Array class implementation is in progress

---
+++ week6/Array.cpp
@@ -0,0 +1,51 @@
+#include "stdafx.h"
+#include "Array.h"
+
+template <typename T>
+Array<T>::Array(size_t size, const T& value) : size_(size) {
+	arr_ = new T[size_];
+	for (size_t i = 0; i < size_; i++)
+		*(arr_ + i) = value;
+}
+
+template <typename T>
+Array<T>::Array(const Array & oArr) : size_(oArr.size_) {
+	arr_ = new T[size_];
+	for (size_t i = 0; i < size_; i++)
+		*(arr_ + i) = *(oArr.arr_ + i);
+}
+
+template <typename T>
+Array<T>::~Array() {
+	delete[] arr_;
+	size_ = 0;
+}
+
+template <class T>
+Array<T>& Array<T>::operator=(const Array<T> & oArr) {
+	if (this != &oArr)
+		Array<T>(oArr).swap(*this);
+	return *this;
+}
+
+template <typename T>
+void Array<T>::swap(Array & oArr) {
+	std::swap(size_, oArr.size_);
+	std::swap(arr_, oArr.arr_);
+}
+
+template <typename T>
+size_t Array<T>::size() const {
+	return size_;
+}
+
+template <typename T>
+T& Array<T>::operator[](size_t index) {
+	return *(arr_ + index);
+}
+
+template <typename T>
+const T& Array<T>::operator[](size_t index) const {
+	return *(arr_ + index);
+}
+
---
+++ week6/Array.h
@@ -0,0 +1,38 @@
+﻿#pragma once
+
+template <typename T>
+class Array
+{
+public:
+	//   конструктор класса, который создает
+	//   Array размера size, заполненный значениями
+	//   value типа T. Считайте что у типа T есть
+	//   конструктор, который можно вызвать без
+	//   без параметров, либо он ему не нужен.
+	explicit Array(size_t size = 0, const T& value = T());
+
+	//   конструктор копирования, который создает
+	//   копию параметра. Считайте, что для типа
+	//   T определен оператор присваивания.
+	Array(const Array &);
+
+	//   деструктор, если он вам необходим.
+	~Array();
+
+	//   оператор присваивания.
+	Array& operator=(const Array &);
+
+	//   возвращает размер массива (количество элементов).
+	size_t size() const;
+
+	//две версии оператора доступа по индексу.
+	T& operator[](size_t);
+	const T& operator[](size_t) const;
+
+private:
+	T * arr_;
+	size_t size_;
+
+	void swap(Array & oArr);
+
+};
